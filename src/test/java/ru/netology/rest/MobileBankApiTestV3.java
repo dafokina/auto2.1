f73bd112960fa5dd90c1380d4a28da590a8a3c1f
Дарья Фокина <cool.russia-33@yandex.ru>
2022-11-04 15:59:57 +0300

    back to rub, add test to check usd

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -23,8 +23,28 @@ class MobileBankApiTestV3 {
           // специализированные проверки - лучше
           .contentType(ContentType.JSON)
           .body("", hasSize(3))
-          .body("[0].currency", equalTo("RUR"))
+          .body("[0].currency", equalTo("RUB"))
           .body("[0].balance", greaterThanOrEqualTo(0))
       ;
+    }
+
+    @Test
+    void shouldReturnUSD() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                // специализированные проверки - лучше
+                .contentType(ContentType.JSON)
+                .body("", hasSize(3))
+                .body("[1].currency", equalTo("USD"))
+                .body("[1].balance", greaterThanOrEqualTo(0))
+        ;
     }
 }
